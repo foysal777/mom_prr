de7477ae7a2ebd021744bcc7f88f6623e1064d9d
Foysal Hossain <118968299+foysal777@users.noreply.github.com>
2026-07-28 16:10:42 +0600

    test

--- notification/migrations/0001_initial.py
+++ notification/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('accounts', '0016_customuser_language'),
+        ('accounts', '0001_initial'),
     ]
 
     operations = [
